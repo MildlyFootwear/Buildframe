8a5f46d1228f99c1adbd53a4355fe8d57204c84f
Shoey <MildlyFootwear>
2026-02-16 16:41:02 -0500

    Version update.

--- Buildframe/Properties/Settings.Designer.cs
+++ Buildframe/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace Buildframe.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v0.2.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v0.2.1")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
