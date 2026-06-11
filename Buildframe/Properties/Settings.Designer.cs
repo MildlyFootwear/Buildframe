44185d9b17124019471be6d2cb9e80c9f110b39d
Shoey <shoeyfiller@gmail.com>
2026-06-11 16:10:55 -0400

    Version update.

--- Buildframe/Properties/Settings.Designer.cs
+++ Buildframe/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Buildframe.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.12.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -61,7 +61,7 @@ namespace Buildframe.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v0.3.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v0.3.2")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
